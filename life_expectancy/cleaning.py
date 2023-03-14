4616a094bbc357395616bb911706e1aa01cb2505
Filipa Alves <filipa.c.alves@nos.pt>
2023-03-14 15:40:23 +0000

    created .gitignore file

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,15 +1,19 @@
 """
-Script to clean data
+Script to load, clean and save data
 """
 import pandas as pd
 import argparse
+import pathlib
+
+# life_path = pathlib.Path.cwd()
+# data_path = life_path.joinpath('data')
 
 def clean_data(path: str, country: str = 'PT'):
     """
     Function to clean data
     """
 
-    df_expectancy = pd.read_csv(path, sep = '\t')
+    df_expectancy = pd.read_csv(path, sep = '\t') # data_path.joinpath(file_name)
 
     new_cols = df_expectancy.columns[0].split(',')
 
@@ -40,3 +44,4 @@ if __name__ == "__main__":
 
 
     clean_data(path ='./life_expectancy/data/eu_life_expectancy_raw.tsv', country = args.country)
+
