6a613cda91eaddcbbf6871c9f80c9e9cad719c66
Filipa Alves <filipa.c.alves@nos.pt>
2023-04-13 17:25:19 +0100

    no mock

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -17,14 +17,13 @@ PT_FILE_PATH = DATA_PATH / PT_FILE_NAME
 def test_main(pt_life_expectancy_expected):
     """Run the main function and compare the output to the expected output"""
 
-    pt_life_expectancy_actual = main('PT').reset_index(drop=True)
+    pt_life_expectancy_actual = main('PT').reset_index(drop = True)
 
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
     )
 
     
-@mock.patch("life_expectancy.data_loading.pd.DataFrame.to_csv")
 def test_load_data(eu_life_expectancy_raw):
 
     """Test load_data function"""
